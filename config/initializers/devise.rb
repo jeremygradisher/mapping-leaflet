550e84ad8a7b9dde0d1870209f0c61644613f3ef
jeremygradisher <jeremygradisher@gmail.com>
2019-12-28 04:52:45 +0000

    Trying to fix error with AWS

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '231 Web Dev App <no-reply@devise-sandbox.herokuapp.com>'
+  config.mailer_sender = '231 Web Dev App <no-reply@mapping-with-leaflet.herokuapp.com>'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
