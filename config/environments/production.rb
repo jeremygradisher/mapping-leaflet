c41c1d9643c8a212b603c3c199e1f97ee0573eb7
jeremygradisher <jeremygradisher@gmail.com>
2022-12-14 03:57:02 +0000

    Chasing a sendgrid error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
+    :domain         => 'mappingleaflet.herokuapp.com'
   }
 
   # Code is not reloaded between requests.
